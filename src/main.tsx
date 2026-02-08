6bd5633f32545ab4ffaf130b517772d284e40017
Andre Boufama <andreboufama@dhcp-vl2041-38966.eduroam.cornell.edu>
2026-02-08 12:18:38 -0500

    Add dev asset studio for image buildings and vector defenses

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { DevAssetStudio } from './devtools/DevAssetStudio.tsx'
+
+const path = window.location.pathname.replace(/\/+$/, '')
+const showDevStudio = path === '/dev/studio'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    {showDevStudio ? <DevAssetStudio /> : <App />}
   </StrictMode>,
 )
